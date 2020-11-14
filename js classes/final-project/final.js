cbbf531977779e0e949888dbb81aa0fcbb9c88f5
alwaysluigi <35355408+alwaysluigi@users.noreply.github.com>
2020-11-14 09:27:44 -0600

    update

--- js classes/final-project/final.js
+++ js classes/final-project/final.js
@@ -2,8 +2,8 @@ const h1 = document.querySelector("h1");
 const letters = h1.innerText.split('');
 let html = "";
 
-letters.forEach(letter => {
-    if(letter !== ' ')
-    html = html + `<span class='letter js=letter'>${letter}</span>`
+letters.forEach(e => {
+    if(e !== ' ')
+    html = html + `<span class='letter js-letter'>${e}</span>`
 })
 h1.innerHTML = html
